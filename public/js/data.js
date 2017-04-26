af1063be430a6b2da7ad2a201bd057eda94c748b
luoxifeng <942394682@qq.com>
2017-04-26 20:26:59 +0800

    change settings and port

--- public/js/data.js
+++ public/js/data.js
@@ -2,25 +2,25 @@ var proArr1 = [
 	{
 		icon: "imgs/github.jpg",
 		name: "自动部署系统",
-		href: "http://luoxifeng.com:2000/",
+		href: "http://webhook.luoxifeng.com/",
 		back: "#333333"
 	},
 	{
 		icon: "imgs/express.jpg",
 		name: "自写express框架",
-		href: "http://luoxifeng.com:2010/",
+		href: "http://express.luoxifeng.com/",
 		back: "#333333"
 	},
 	{
 		icon: "imgs/weichat.jpg",
 		name: "简易聊天室",
-		href: "http://luoxifeng.com:2020/",
+		href: "http://chatroom.luoxifeng.com/",
 		back: "#333333"
 	},
 	{
 		icon: "imgs/maoyan.jpg",
 		name: "喵喵眼（仿照猫眼）",
-		href: "http://luoxifeng.com:2030/",
+		href: "http://miaomiaoyan.luoxifeng.com/",
 		back: "#333333"
 	},
 	
@@ -32,14 +32,14 @@ var proArr2 = [
 	{
 		icon: "imgs/utils.png",
 		name: "前端工具(很快上线)",
-		href: "http://luoxifeng.com:2040/",
+		href: "http://feutils.luoxifeng.com:2040/",
 		back: "#333333",
 		usable: false
 	},
 	{
 		icon: "imgs/vue.png",
 		name: "open-ui(vue组件)(暂未上线)",
-		href: "http://luoxifeng.com:2050/",
+		href: "http://openui.luoxifeng.com/",
 		back: "#333333",
 		usable: false
 	}
